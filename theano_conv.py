f03b57fc2347557f0761d102e7bac8e095dc7291
reyhaneaskari <r.askari.hemmat@gmail.com>
2018-01-29 20:02:57 +0000

    fi

--- theano_conv.py
+++ theano_conv.py
@@ -4,24 +4,26 @@ import theano.tensor as T
 import time
 
 x = T.tensor4('x')
+x = theano.shared(
+    np.random.rand(32, 128, 256, 256).astype(theano.config.floatX),
+    'x')
 filters = theano.shared(
-    np.random.rand(256, 128, 8, 8).astype(theano.config.floatX),
+    np.random.rand(256, 128, 3, 3).astype(theano.config.floatX),
     'filters')
 # B x 1 x 1 x T
 y = theano.gpuarray.dnn.dnn_conv(
     img=x,
     kerns=filters,
     border_mode='half',
-    precision=theano.config.floatX)
+    precision='float32')
 
-f = theano.function([x], y)
-x_ = np.random.rand(32, 128, 256, 256).astype(theano.config.floatX)
-y_ = f(x_)
+f = theano.function([], y)
+y_ = f()
 #f.sync_shared()
 t0 = time.time()
 for i in range(50):
     print i
-    y_ = f(x_)
+    y_ = f()
  #   f.sync_shared()
 t1 = time.time()
 print t1 - t0
