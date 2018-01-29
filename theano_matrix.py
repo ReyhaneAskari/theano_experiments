9eca7b9a74f4dcf8dc45e690efae238efdb9c279
reyhaneaskari <r.askari.hemmat@gmail.com>
2018-01-29 17:24:37 +0000

    updated fmatrix to matrix

--- theano_matrix.py
+++ theano_matrix.py
@@ -3,13 +3,13 @@ import theano.tensor as T
 import numpy as np
 import time
 
-x_1 = T.fmatrix("x_1")
-x_2 = T.fmatrix("x_2")
+x_1 = T.matrix("x_1", dtype=theano.config.floatX)
+x_2 = T.matrix("x_2", dtype=theano.config.floatX)
 y = T.dot(x_1, x_2)
 
 f = theano.function([x_1, x_2], y)
-x_1_ = np.random.rand(256, 256).astype(theano.config.floatX)
-x_2_ = np.random.rand(256, 256).astype(theano.config.floatX)
+x_1_ = np.random.rand(4096, 4096).astype(theano.config.floatX)
+x_2_ = np.random.rand(4096, 4096).astype(theano.config.floatX)
 
 y_ = f(x_1_, x_2_)
 t0 = time.time()
